6deab80af0c7bd33f0d735a849f2ca7f239a3171
lucasquearis <lucasquearis@hotmail.com>
2022-02-06 16:38:53 -0300

    estiliza pagina com user nao logado

--- src/components/MessageError/index.jsx
+++ src/components/MessageError/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { actionError } from '../../redux/actions/app';
 import ConditionalComponent from '../ConditionalComponent';
+import './style.css';
 
 function Message() {
   const dispatch = useDispatch();
@@ -12,13 +13,15 @@ function Message() {
   };
 
   return (
-    <ConditionalComponent className="already-exist" condition={error}>
-      <h1>
-        {error}
+    <ConditionalComponent className="already-exist" condition={!!error}>
+      <div className="error-box">
+        <h1>
+          {error}
+        </h1>
         <button type="button" onClick={() => confirm()}>
           OK
         </button>
-      </h1>
+      </div>
     </ConditionalComponent>
   );
 }
--- src/components/notFoundUserName/index.jsx
+++ src/components/notFoundUserName/index.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { actionUserList } from '../../redux/actions/socket';
 import { HOME_PATH } from '../../constants/list';
+import './style.css';
 
 function NotFoundUserName() {
   const { socket } = useSelector((state) => state.socketReducer);
@@ -13,13 +14,15 @@ function NotFoundUserName() {
   }, []);
 
   return (
-    <div>
-      <h1>Você não está logado, por favor clique no botão para logar corretamente</h1>
-      <Link to={HOME_PATH}>
-        <button type="button">
-          Logar
-        </button>
-      </Link>
+    <div className="no-user-container">
+      <div className="no-user-box">
+        <h1>Você não está logado, por favor clique no botão para logar corretamente</h1>
+        <Link to={HOME_PATH}>
+          <button type="button">
+            Logar
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
